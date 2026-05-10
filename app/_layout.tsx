278b742fb677d7610807147bbaaee4b03899d595
Alex Xiang <axiang1307@gmail.com>
2026-05-09 22:03:28 -0400

    phone header to black

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout() {
             <Stack.Screen name="settings" />
             <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
           </Stack>
-          <StatusBar style="auto" />
+          <StatusBar style="dark" />
         </ThemeProvider>
       </AuthProvider>
     </SafeAreaProvider>
